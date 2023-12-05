c453a46c97051d6bddf49de17ad6f320b7c467c7
Joaovitor1228 <vitorgatinho771@gmail.com>
2023-12-05 03:34:22 -0300

    completar tarefas no banco de dados

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ app.use(express.urlencoded({
     extended: true
 }))
 
+app.post('/completar', (requisicao, resposta)=>{
+    const id = requisicao.body.id
+
+    const sql = `
+         UPDATE tarefas
+         SET completa = '1'
+         WHERE id = ${id}
+ 
+    `
+              
+    conexao.query(sql, (erro)=>{
+        if (erro){
+            return console.log(erro)
+        }
+        resposta.redirect('/')
+})
+})
+
 app.post('/criar', (requisicao, resposta)=>{
     const descricao = requisicao.body.descricao
     const completa = 0
